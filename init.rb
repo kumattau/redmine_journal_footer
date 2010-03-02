17bdc68b82e07897e3d855f47232ac1e809b6fe6
kumattau <kumattau@gmail.com>
2010-03-02 19:30:20 +0900

    version up

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_journal_footer do
   name 'Redmine Journal Footer plugin'
   author 'kumattau'
   description 'This is a journal footer plugin for Redmine'
-  version '0.0.1'
+  version '0.0.2'
 
   project_module :issue_tracking do |map|
     permission(
